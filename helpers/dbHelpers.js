cee52d9002a92ecc5343500d4605ff3dc25af17f
javascript01fan <ejrojas@icloud.com>
2019-11-28 16:56:58 -0500

    initial commit

---
+++ helpers/dbHelpers.js
@@ -0,0 +1,72 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+module.exports = pool => {
+  //get all users
+  const getUsers = (request, response) => {
+    console.log("getUser dbhelpers");
+    const query = {
+      text: "SELECT * FROM users ORDER BY id ASC"
+    };
+    return pool.query(query);
+  };
+  //get user by id
+  const getUserById = id => {
+    const query = {
+      text: "SELECT * FROM users WHERE id = $1",
+      values: [id]
+    };
+    return pool.query(query);
+  };
+  //update user
+  const updateUser = (first_name, last_name, email, password, age, bio, height, image_url, location, weight, id) => {
+    const hashPassword = bcrypt.hashSync(password, 10);
+    const query = {
+      text: "UPDATE users SET first_name = $1, last_name = $2, email = $3, password = $4, age= $5, bio= $6, height= $7, image_url= $8, location= $9, weight= $10 WHERE id = $11",
+      values:  [first_name,last_name,email,hashPassword, age, bio, height, image_url, location, weight, id]
+    };
+    return pool.query(query);
+  };
+  const addUser = (firstName, lastName, email, password) => {
+    // console.log("requested email", request.body.email);
+    //---------
+    const query = {
+      text: "SELECT * FROM users WHERE email = $1",
+      values: [email]
+    };
+    return pool.query(query).then(result => {
+      //console.log(result.rows[0].id)
+      if (result.rowCount === 0) {
+        const hashPassword = bcrypt.hashSync(password, 10);
+        //  const userId = result.rows[0].id;
+        // console.log('>>>>',result.rows)
+
+        const insertQuery = {
+          text:
+            "INSERT INTO users (first_name,last_name, email,password) VALUES ($1, $2, $3, $4) RETURNING id",
+          values: [firstName, lastName, email, hashPassword]
+        };
+        return pool.query(insertQuery);
+      } else {
+        return { message: "Email Aready in Use!" };
+      }
+    });
+  };
+  //login user
+  const loginUser = (email) => {
+    const query = {
+      text: "SELECT * FROM users WHERE email = $1",
+      values: [email]
+    };
+    return pool.query(query);
+  };
+
+  return {
+    getUsers,
+    getUserById,
+    updateUser,
+    addUser,
+    loginUser,
+  
+  };
+};
---
+++ routes/users.js
@@ -0,0 +1,119 @@
+var express = require("express");
+var router = express.Router();
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+module.exports = dbHelpers => {
+
+  router.get('/user-info', function (req, res) {
+    console.log('test')
+    const { authorization} = req.headers;
+    const [, token] = authorization.split(" ");
+     //extracting the payload  example {userId: 1}
+    const payload = jwt.verify(token, process.env.SECRET);
+    console.log(payload)
+    dbHelpers
+    .getUserById(payload.userID)
+      .then(result => {
+      res.send(result.rows[0])
+    })
+  })
+
+  /* GET users listing. */
+  router.get("/", function(req, res) {
+    console.log("getting users");
+    dbHelpers
+      .getUsers()
+      .then(result => {
+        res.send(result.rows);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  });
+  router.get("/:id", function(req, res) {
+    const id = parseInt(req.params.id);
+    dbHelpers
+      .getUserById(id)
+      .then(result => {
+        res.send(result.rows);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  });
+  router.put("/:id", function(req, res) {
+    const id = parseInt(req.params.id);
+    console.log(req.body);
+    const {first_name,last_name,email,password, age, bio, height, image_url, location, weight} = req.body;
+   
+    dbHelpers
+      .updateUser(first_name,last_name,email,password, age, bio, height, image_url, location, weight, id)
+      .then(result => {
+        //if in query $2 RETURNING * use result.rows as an answer.
+        res.status(200).send(`User modified with ID: ${id}`);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  });
+  router.post("/", function(req, res) {
+    const { first_name, last_name, email, password } = req.body;
+
+    dbHelpers
+      .addUser(first_name, last_name, email, password)
+      .then(result => {
+        if (result.rows) {
+          const userId = result.rows[0].id;
+          const token = jwt.sign({ userId }, process.env.SECRET);
+          res.status(200).json({
+            message: "You are logged in!!",
+            token: token
+          });
+        } else {
+          res.status(200).json({
+            message: result.message
+          });
+        }
+        // console.log(result)
+        // res.status(200).send({message:'Signup Successfuly ',token:token})
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  });
+  router.post("/login", function(request, response) {
+    const { email, password } = request.body;
+    dbHelpers.loginUser(email).then(res => {
+      if (res.rowCount === 1) {
+        bcrypt.compare(password, res.rows[0].password, (err, match) => {
+          if (match) {
+            const token = jwt.sign(
+              { userID: res.rows[0].id },
+              process.env.SECRET
+            );
+            response.send({ message: "You are logged in!", token: token });
+          } else {
+            response.send({ message: "Invalid Password!!" });
+          }
+        });
+      } else {
+        response.send({ message: "No email found!!" });
+      }
+    });
+  });
+
+  // router.get('/test', function (req, res) {
+  // console.log('test')
+  //   //const { authorization} = req.headers;
+  //   //const [, token] = authorization.split(" ");
+  //    // extracting the payload  example {userId: 1}
+  //   //const payload = jwt.verify(token, process.env.SECRET);
+  //   //console.log(payload)
+  //   // dbHelpers
+  //   // .getUserById(payload.userID)
+  //   //   .then(result => {
+  //   //   console.log(result)
+  //   // })
+  // })
+  return router;
+};
