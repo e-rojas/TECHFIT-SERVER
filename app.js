557fc6937d739822d23d9e529abe7f03ffe3c21d
anthonyshadow <anthony.shadowitz@gmail.com>
2019-11-29 15:06:05 -0500

    create dbhelpers and queries for workout page, created workout api route

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const dbHelpers = require("./helpers/dbHelpers")(pool);
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
 const mealsRouter = require("./routes/meals");
+const workoutsRouter = require("./routes/workouts")
 const bodyParser = require("body-parser");
 
 const app = express();
@@ -25,7 +26,8 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/api/meals", mealsRouter(dbHelpers));
-app.use("/api/users", usersRouter(dbHelpers));
+app.use("/api/users", usersRouter(dbHelpers))
+app.use("/api/workouts", workoutsRouter(dbHelpers));
 
 
 // catch 404 and forward to error handler
